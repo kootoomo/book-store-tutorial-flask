06d26caef614c2f0bee09a2d1425111b2d66d830
Elekoot <kootoomo@gmail.com>
2019-03-13 00:03:57 +0300

    basedir added

--- settings.py
+++ settings.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/book-store-tutorial-flask/book-store-tutorial-flask/database.db'
+basedir ='/var/www/book-s/book-store-tutorial-flask/'
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/book-s/book-store-tutorial-flask/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(basedir, 'database.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
